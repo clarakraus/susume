14e681fabe9be3ded92d0e039ebf3087e89040c9
Clara Kraus <claramariakrausgmail.com>
2022-07-08 22:09:18 +0200

    Unit test shouldCreateNewBlog in BlogServiceTest is up and running

---
+++ backend/src/test/java/com/github/clarakraus/susume/blog/BlogServiceTest.java
@@ -0,0 +1,22 @@
+package com.github.clarakraus.susume.blog;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogServiceTest {
+
+    @Test
+    void shouldCreateNewBog(){
+        BlogRepo testRepo = Mockito.mock(BlogRepo.class);
+        Blog testBlog = new Blog(null, "testname", "testDescription", "LinkToPicture");
+        BlogService testService = new BlogService(testRepo);
+
+        testService.createBlog(testBlog);
+
+        Mockito.verify(testRepo).save(testBlog);
+
+    }
+
+}
