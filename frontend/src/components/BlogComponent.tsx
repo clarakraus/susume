3c7dafa45fa3ec267df78e3c7c30d2bf485f56f3
Clara Kraus <claramariakrausgmail.com>
2022-07-25 17:12:33 +0200

    redirecting to login when no token

--- frontend/src/components/BlogComponent.tsx
+++ frontend/src/components/BlogComponent.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {getFriendBlogDetails, getProfileDetails, sendFriendsList} from "../service/BlogService";
 import SearchFriends from "./SearchFriends";
 import {FriendItem} from "../service/Model";
@@ -15,7 +15,7 @@ export default function BlogComponent(props: BlogNameProps){
     const [profilePicture, setProfilePicture] = useState("")
     const [profileDescription, setProfileDescription] = useState("")
     const [friendList, setFriendList] = useState<Array<FriendItem>>([])
-  //  const nav = useNavigate()
+    const nav = useNavigate()
  //   const [errorCode, setErrorCode] = useState("")
 
 
@@ -32,12 +32,12 @@ export default function BlogComponent(props: BlogNameProps){
                     sendFriendsList(data.friendsList!)
                         .then(data => setFriendList(data))
                 })
- /*              .catch((e) => {
-                    if(e.status === 403){
+               .catch((e) => {
+                    if(e.response.status === 403){
                         nav("/")
                     }
                 })
-  */
+
         }
         else {
             getFriendBlogDetails(username!)
