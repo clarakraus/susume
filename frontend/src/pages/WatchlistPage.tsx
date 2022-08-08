54aa406ddbb5056d0555c5793e7244aa6479bdb4
Clara Kraus <claramariakrausgmail.com>
2022-08-08 10:39:40 +0200

    added dependency

--- frontend/src/pages/WatchlistPage.tsx
+++ frontend/src/pages/WatchlistPage.tsx
@@ -20,7 +20,7 @@ export default function WatchlistPage(){
                         nav("/")
                     }
                 })
-    }, [])
+    }, [nav])
 
    useEffect(() =>{
       refreshSusumeWatchlist()
